5d1b6d58e89d50000119d294b67959ee1f60b492
Shreyash Bandi <Shreyash.Bandi@cdk.com>
2025-07-06 22:29:50 +0530

    Done with FrontEnd Setup

---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
+const SignUpPage = () => {
+  return (
+    <div>SignUpPage</div>
+  )
+}
+
+export default SignUpPage
